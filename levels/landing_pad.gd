2d109dc8b209581847bfc105093935146f8501c2
Ben Guo <benjaminguo14@gmail.com>
2025-11-16 21:45:22 +1100

    20

---
+++ levels/landing_pad.gd
@@ -0,0 +1,14 @@
+extends CSGBox3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+@export_file("*.tscn")
+var level_complete_scene: String
